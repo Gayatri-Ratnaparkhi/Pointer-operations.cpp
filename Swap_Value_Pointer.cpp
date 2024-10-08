7fb4631567c027d72e169a9323ca365cf9704b6c
Gayatri <gayatriratnaparkhi3@gmail.com>
2024-10-08 13:08:18 +0530

    Add files via upload

---
+++ Swap_Value_Pointer.cpp
@@ -0,0 +1,33 @@
+//gayatri ratnaparkhi 23070123169
+
+#include <iostream>
+
+using namespace std;
+
+    int main(){
+    	
+        int a=10,b=20;
+        
+        int temp;
+        
+        temp = a;
+        
+        a = b;
+        
+        b = temp;
+        
+        cout<<"a = "<<a<<endl;
+        
+        cout<<"b = "<<b<<endl;
+        
+    }
+    
+/*
+
+**OUTPUT**
+
+a = 20
+
+b = 10
+
+*/
